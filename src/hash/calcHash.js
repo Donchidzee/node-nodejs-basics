49ef9c6841d05a9f8bdd5833ea8a696dc50317a7
Donchidzee <donchik020202@gmail.com>
2024-10-07 23:34:25 +0200

    feat: update calcHash.js file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,35 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+import { join } from "node:path";
+
+const calcHash = async () => {
+  const filePath = join(
+    process.cwd(),
+    "src",
+    "hash",
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  try {
+    const hash = createHash("sha256");
+    const stream = createReadStream(filePath);
+
+    stream.on("data", (chunk) => {
+      hash.update(chunk);
+    });
+
+    stream.on("end", () => {
+      const fileHash = hash.digest("hex");
+      console.log(`SHA256 hash (hex): ${fileHash}`);
+    });
+
+    stream.on("error", (error) => {
+      throw new Error("FS operation failed");
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
-await calculateHash();
+await calcHash();
