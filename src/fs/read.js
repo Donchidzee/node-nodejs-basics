99c14a0ed8f9d78b90532ef1804bde6da9cf1363
Donchidzee <donchik020202@gmail.com>
2024-10-07 22:51:22 +0200

    feat: update read.js file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import { join } from "node:path";
+
 const read = async () => {
-    // Write your code here 
+  const fileToRead = join(
+    process.cwd(),
+    "src",
+    "fs",
+    "files",
+    "fileToRead.txt"
+  );
+
+  try {
+    await fs.access(fileToRead);
+    const content = await fs.readFile(fileToRead, "utf-8");
+    console.log(content);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
-await read();
+await read()
+  .then(() => console.log("File read successfully"))
+  .catch((error) => console.error(error.message));
