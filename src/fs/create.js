5e17b0bc7d18144220281b95921fdc5e08234be6
Donchidzee <donchik020202@gmail.com>
2024-10-07 17:04:07 +0200

    feat: update create.js file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from "node:fs/promises";
+import { join } from "node:path";
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const filePath = join(process.cwd(), "src", "fs", "files", "fresh.txt");
+
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.writeFile(filePath, content);
+    } else {
+      throw error;
+    }
+  }
 };
 
-await create();
+create()
+  .then(() => console.log("File created successfully"))
+  .catch((error) => console.error(error.message));
