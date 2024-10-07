91e95c6229c4c052681e987e88dc67c74c0ae7d4
Donchidzee <donchik020202@gmail.com>
2024-10-08 00:36:37 +0200

    feat: update streams write.js file

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from "node:fs";
+
+const write = () => {
+  const writableStream = createWriteStream(
+    "./src/streams/files/fileToWrite.txt"
+  );
+
+  process.stdin.pipe(writableStream);
+
+  writableStream.on("finish", () => {
+    console.log("Data has been written to fileToWrite.txt successfully");
+  });
+
+  writableStream.on("error", (error) => {
+    console.error("Error writing to file:", error.message);
+  });
 };
 
-await write();
+write();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,41 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { join } from "node:path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = cpus().length;
+  const startNumber = 10;
+  const results = new Array(numCPUs);
+  const filePath = join(process.cwd(), "src", "wt", "worker.js");
+
+  const promises = Array.from({ length: numCPUs }, (_, index) => {
+    const worker = new Worker(filePath);
+    const numberToSend = startNumber + index;
+
+    return new Promise((resolve) => {
+      worker.postMessage(numberToSend);
+
+      worker.on("message", (result) => {
+        results[index] = { status: "resolved", data: result };
+        resolve();
+      });
+
+      worker.on("error", () => {
+        results[index] = { status: "error", data: null };
+        resolve();
+      });
+
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          results[index] = { status: "error", data: null };
+          resolve();
+        }
+      });
+    });
+  });
+
+  await Promise.all(promises);
+  console.log(results);
 };
 
 await performCalculations();
